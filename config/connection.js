32cf7310626af23c30375f95c62b1fc6de9ec3bd
miao0007 <miao188@hotmail.com>
2020-08-15 16:52:06 +0930

    fixed heroku connection problem

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,20 @@
 // Set up MySQL connection
 const mysql = require("mysql");
+let connection;
 
-let connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user:"root",
-    password:"huanXI861109",
-    database:"burgers_db"
-});
+// build connection to JAWSDB for Heroku
+if (process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+  } else {
+    connection = mysql.createConnection({
+      host: "localhost",
+      port: 3306,
+      user: "root",
+      password: "huanXI861109",
+      database: "burgers_db"
+    });
+  
+  }
 
 // make connection
 connection.connect(function(err){
